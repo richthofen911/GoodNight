58aa0d957098ab57bb104d7a768c9f050efbce96
richthofen911 <yichaoli.richthofen@gmail.com>
2015-09-18 22:52:41 -0400

    init

---
+++ app/src/main/java/net/callofdroidy/goodnight/ActivityRegister.java
@@ -0,0 +1,23 @@
+package net.callofdroidy.goodnight;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class ActivityRegister extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+
+
+}
+
